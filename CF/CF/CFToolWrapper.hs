f83abf791b68ed2e12aa5e9fab54a941adda96a2
ahidalgob <ahidalgo94@gmail.com>
2019-09-16 16:28:26 -0400

    submitSolution tests with samples

--- CF/CF/CFToolWrapper.hs
+++ CF/CF/CFToolWrapper.hs
@@ -1,23 +1,22 @@
-module CF.CFToolWrapper where
+module CF.CFToolWrapper(getInputOutput, submitSolution) where
 
-import           Control.Monad                  ( liftM2 )
+import Control.Monad( liftM2 )
 
-import           Data.Char                      ( toLower )
-import           Data.List                      ( partition
-                                                , isPrefixOf
-                                                , sort
-                                                )
+import Data.Char( toLower )
+import Data.List( partition, isPrefixOf, sort )
 
-import           System.Directory               ( removePathForcibly
-                                                , renameDirectory
-                                                , listDirectory
-                                                )
-import           System.FilePath.Posix          ( (</>) )
-import           System.Process                 ( readProcess )
+import System.Directory( removePathForcibly, renameDirectory ,listDirectory )
+import System.FilePath.Posix( (</>) )
+import System.Process( readProcess )
+import System.Posix.Directory( getWorkingDirectory, changeWorkingDirectory )
 
+data Verdict = Accepted | Rejected deriving Show
+
+problemIdToStrings :: (Int, Char) -> (String, String)
+problemIdToStrings (cIdInt, pIdChar) = (show cIdInt, [toLower pIdChar])
 
 getInputOutput :: (Int, Char) -> IO [(String, String)]
-getInputOutput (cIdInt, pIdChar) = do
+getInputOutput problemId = do
   _ <- readProcess "./cf-tool/cf" ["parse", cId, pId] ""
 
   removePathForcibly $ "./inputs" </> cId </> pId
@@ -27,9 +26,17 @@ getInputOutput (cIdInt, pIdChar) = do
   let (ins, outs) = partition ("in" `isPrefixOf`) dir
   liftM2 zip (mapM readFile' $ sort ins) (mapM readFile' $ sort outs)
   where
-    cId :: String
-    cId = show cIdInt
-    pId :: String
-    pId = [toLower pIdChar]
+    (cId, pId) = problemIdToStrings problemId
     readFile' :: FilePath -> IO String
     readFile' fp = readFile $ "./inputs" </> cId </> pId </> fp
+
+submitSolution :: (Int, Char) -> FilePath -> IO Verdict
+submitSolution problemId fp = do
+  let (cId, pId) = problemIdToStrings problemId
+
+  changeWorkingDirectory $ "./inputs" </> cId </> pId
+  output <- readProcess "../../../cf-tool/cf" ["parse", cId, pId] ""
+  print output
+
+  changeWorkingDirectory "../../.."
+  return Accepted
