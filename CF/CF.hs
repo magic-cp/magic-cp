2796781b95b4d90ca00650195671a06e451e37c9
ahidalgob <ahidalgo94@gmail.com>
2019-09-19 23:20:04 -0400

    getPredicate

--- CF/CF.hs
+++ CF/CF.hs
@@ -4,9 +4,17 @@ module CF where
 import Data.Maybe
 
 import CF.CFAPI
+import CF.CFConfig
+import CF.CFToolWrapper
 
 type ProblemId = (Int, Char)
 data Problem = Problem {problemId :: ProblemId, rating :: Int} deriving Show
+
+getPredicate :: CFConfig -> ProblemId -> IO ((String -> String) -> Bool)
+getPredicate cfg problemId = do
+  inputOutput <- getInputOutput cfg problemId
+  print inputOutput
+  return $ \f -> and [f i == o | (i, o) <- inputOutput]
 
 getProblems :: IO [Problem]
 getProblems =  map apiProbToProb <$> getAPIProblems
